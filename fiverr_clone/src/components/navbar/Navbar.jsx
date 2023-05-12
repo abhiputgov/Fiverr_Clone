95f986584cf77fa3b7d36fb0876c3d5721e4d773
abhiputgov <abhipg@outlook.com>
2023-05-12 14:16:01 +0530

    navbar detailing

--- fiverr_clone/src/components/navbar/Navbar.jsx
+++ fiverr_clone/src/components/navbar/Navbar.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Navbar.scss';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [active, setActive] = useState(false);
+  return (
+    <div className={active ? 'navbar active' : 'navbar'}>
+      <div className="container">
+        <div className="logo">
+          <span className="text">fiverr</span>
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign In</span>
+          <span>Become a Seller</span>
+          <button className="joinBtn">Join</button>
+        </div>
+      </div>
+      <hr />
+      <div className="menu">
+        <span>Test</span>
+        <span>Test2</span>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

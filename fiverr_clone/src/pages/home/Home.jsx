56f403e37d905de4e963975ff65c93f997640a31
abhiputgov <abhipg@outlook.com>
2023-05-22 16:51:36 +0530

    inifnite scroll of category cards

--- fiverr_clone/src/pages/home/Home.jsx
+++ fiverr_clone/src/pages/home/Home.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import './Home.scss';
 import Featured from '../../components/featured/Featured';
 import TrustedBy from '../../components/trustedBy/TrustedBy';
+import Slide from '../../components/slide/Slide';
+import CatCard from '../../components/catcard/catCard';
+import { cards } from '../../data';
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
     </div>
   );
 };

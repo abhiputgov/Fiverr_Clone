1f3738d6128d9c4d9b1870752e3a62ae2e1b7a80
abhiputgov <abhipg@outlook.com>
2023-05-22 23:25:24 +0530

    adding featured design and slider carousel ad updating them

--- fiverr_clone/src/pages/home/Home.jsx
+++ fiverr_clone/src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './Home.scss';
-import Featured from '../../components/featured/Featured';
-import TrustedBy from '../../components/trustedBy/TrustedBy';
-import Slide from '../../components/slide/Slide';
-import CatCard from '../../components/catcard/catCard';
-import { cards } from '../../data';
+import React from "react";
+import "./Home.scss";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
+import CatCard from "../../components/catcard/catCard";
+import { cards } from "../../data";
 
 const Home = () => {
   return (
@@ -16,6 +16,53 @@ const Home = () => {
           <CatCard key={card.id} card={card} />
         ))}
       </Slide>
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h1>A whole world of freelance talent at your fingertips</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="check" />
+              The best of every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="check" />
+              The best of every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="check" />
+              The best of every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="check" />
+              The best of every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing
+            </p>
+          </div>
+          <div className="item">
+            <video controls="true">
+              <source
+                src="https://www.youtube.com/watch?v=KI5CnJ9u5ok"
+                type="video/mp4"
+              />
+            </video>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

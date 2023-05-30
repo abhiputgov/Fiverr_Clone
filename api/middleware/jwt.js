d1b3771a1db60e6c1c1e8b71c0b78412eef7d6b7
abhiputgov <abhipg@outlook.com>
2023-05-31 01:26:36 +0530

    middleware error fixing (forgot next call)

--- api/middleware/jwt.js
+++ api/middleware/jwt.js
@@ -1,16 +1,18 @@
-const verifyToken = (req, res) => {
+const jwt = require("jsonwebtoken");
+const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
   if (!token) {
     res.status(401).send("You are not authenticated");
   }
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
     if (err) {
-      res.status(403).send("Token is invalid");
+      return res.status(403).send("Token is invalid");
     }
 
     req.userId = payload.id;
     req.isSeller = payload.isSeller;
+    next();
   });
 };
 
-module.exports = verifyToken;
+module.exports = { verifyToken };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { deleteUser } = require("../controllers/user.controller");
-const verifyToken = require("../middleware/jwt");
+const { verifyToken } = require("../middleware/jwt");
 const router = express.Router();
 
 router.delete("/:id", verifyToken, deleteUser);

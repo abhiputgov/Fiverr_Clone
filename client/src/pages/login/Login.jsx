db5fe170bde9cefcd260726040bbd3360848e44f
abhiputgov <abhipg@outlook.com>
2023-05-31 16:03:52 +0530

    updating the login through node and client

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import './Login.scss';
+import { useRef } from 'react';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const handleSubmit = () =>{};
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          type="text"
+          name="username"
+          placeholder="For Eg. Joe Doe"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;

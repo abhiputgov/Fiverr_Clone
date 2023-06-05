dd333f8a00da86195f8c65ed0536cfdb1f0ac308
abhiputgov <abhipg@outlook.com>
2023-06-06 00:27:52 +0530

    adding registeration of users

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react';
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Add from './pages/add/Add';
-import Gig from './pages/gig/Gig';
-import Gigs from './pages/gigs/Gigs';
-import Home from './pages/home/Home';
-import Message from './pages/message/Message';
-import Messages from './pages/messages/Messages';
-import MyGigs from './pages/myGigs/MyGigs';
-import Orders from './pages/orders/Orders';
-import Login from './pages/login/Login';
-import './App.scss';
+import { useState } from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Add from "./pages/add/Add";
+import Gig from "./pages/gig/Gig";
+import Gigs from "./pages/gigs/Gigs";
+import Home from "./pages/home/Home";
+import Message from "./pages/message/Message";
+import Messages from "./pages/messages/Messages";
+import MyGigs from "./pages/myGigs/MyGigs";
+import Orders from "./pages/orders/Orders";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import "./App.scss";
 
 function App() {
   const Layout = () => {
@@ -26,18 +27,19 @@ function App() {
 
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: "/",
       element: <Layout />,
       children: [
-        { path: '/', element: <Home /> },
-        { path: '/gigs', element: <Gigs /> },
-        { path: '/gig/:id', element: <Gig /> },
-        { path: '/orders', element: <Orders /> },
-        { path: '/mygigs', element: <MyGigs /> },
-        { path: '/addGig', element: <Add /> },
-        { path: '/messages', element: <Messages /> },
-        { path: '/message/:id', element: <Message /> },
-        { path: '/login', element: <Login /> },
+        { path: "/", element: <Home /> },
+        { path: "/gigs", element: <Gigs /> },
+        { path: "/gig/:id", element: <Gig /> },
+        { path: "/orders", element: <Orders /> },
+        { path: "/mygigs", element: <MyGigs /> },
+        { path: "/addGig", element: <Add /> },
+        { path: "/messages", element: <Messages /> },
+        { path: "/message/:id", element: <Message /> },
+        { path: "/login", element: <Login /> },
+        { path: "/register", element: <Register /> },
       ],
     },
   ]);
